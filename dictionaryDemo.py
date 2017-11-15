4c946f57425818345ba12a152c9e8eed8643012d
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-15 13:35:46 -0500

    Updated from Brython Server: 11/15/2017 1:35:45 PM

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,10 @@
+#Jack Robey
+#11/15/17
+#dictionaryDemo.py - list practice
 
+dictionary = ['alphabet','sweatshirt','sweatpants','shorts','computer','water']
+
+dictionary.sort()
+
+number = int(input('What number word do you want to look up? '))
+print('Word number', number, 'is', dictionary[number-1])

e7274c75c4e546f25e8d5dde6565dcfe21e5bf4c
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-15 13:46:14 -0500

    Updated from Brython Server: 11/15/2017 1:46:14 PM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,11 @@
+#Jack Robey
+#11/15/17
+#middleWord.py - asks the user for a list of words then prints out the middle one
+
+words = input('Enter some words: ').split(' ')
+
+middleIndexing = len(words)/2
+
+print(words[middleIndexing])
+
 

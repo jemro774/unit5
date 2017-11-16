521184b2fedff5752bc6d3b082bdd45a406168e9
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-16 14:34:52 -0500

    Updated from Brython Server: 11/16/2017 2:34:50 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,11 @@
+#Jack Robey
+#11/16/17
+#warmup13 - makes a list of 20 random numbers and does stuff with it
 
+from random import randint
+
+numbers = []
+
+for numbers in range(1,21):
+    numbers.append(randint(1,101))
+    

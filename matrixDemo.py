3251ef1c9afe5fe630b85f487e4a02fc9a075ae3
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-17 14:29:18 -0500

    Updated from Brython Server: 11/17/2017 2:29:17 PM

--- matrixDemo.py
+++ matrixDemo.py
@@ -1,1 +1,12 @@
+#Jack Robey
+#11/17/17
+#matrixDemo.py - how to create/use a matrix
+
+board = [['a','b','c'],['d','e','f'],['g','h','i']]
+
+def printBoard():
+    for row in range(0,3):
+        for col in range(0,3):
+            print(board[row][col],' ',end = '')
+        print()
 

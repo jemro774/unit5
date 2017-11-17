30a18023c35996a682544b3b1a13c771f2bb6dec
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-17 14:55:18 -0500

    Updated from Brython Server: 11/17/2017 2:55:17 PM

--- randomMonth.py
+++ randomMonth.py
@@ -1,1 +1,34 @@
+#Jack Robey
+#11/17/17
+#randomMonth.py - prints out a random month of the year
+
+from random import randint
+
+month = randint(1,12)
+
+if month == 1:
+    print('January')
+elif month == 2:
+    print('February')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+elif month == 1:
+    print('January')
+else:
+    print('January')
+
 

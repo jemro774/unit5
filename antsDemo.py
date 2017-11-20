029ff17043883d827ca2623c85ff76e2a0ab48f6
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-20 14:33:55 -0500

    Updated from Brython Server: 11/20/2017 2:33:54 PM

--- antsDemo.py
+++ antsDemo.py
@@ -8,12 +8,24 @@ from random import randint
 WIDTH = 800
 HEIGHT = 400
 
+def step():
+    for ant in data['antList']:
+        dx = randint(-4,3)
+        dy = randint(-4,3)
+        if ant.x + dx > 0 and ant.x + dx < WIDTH:
+            ant.x += dx
+        if ant.y + dy > 0 and ant.y + dy < HEIGHT:
+            ant.y += dy
+
 if __name__ == '__main__':
     
     red = Color(0xFF0000,1)
     ant = CircleAsset(10,LineStyle(1,red),red)
     
-    for i in range(10):
-        Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT)))
+    data = {}
+    data['antList'] = []
     
-    App().run()
+    for i in range(10):
+        data['antList'].append(Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT))))
+    
+    App().run(step)

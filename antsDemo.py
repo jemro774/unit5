8e3a9a144acf1bb8bcdac78e46a4e14058e0134e
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-20 14:26:50 -0500

    Updated from Brython Server: 11/20/2017 2:26:50 PM

--- antsDemo.py
+++ antsDemo.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     red = Color(0xFF0000,1)
     ant = CircleAsset(10,LineStyle(1,red),red)
     
-    Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT)))
+    for i in range(10):
+        Sprite(ant,(randint(1,WIDTH),randint(1,HEIGHT)))
     
     App().run()

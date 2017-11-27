5ec931e601dde9cf5732fce120b926811f1712a7
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-27 14:39:11 -0500

    Updated from Brython Server: 11/27/2017 2:39:11 PM

--- cocktailSort.py
+++ cocktailSort.py
@@ -1,15 +1,43 @@
 #Jack Robey
 #11/27/17
-#sorting.py - code to test a sorting function
+#cocktailSort.py - implementation of cocktail sort
 
 from random import randint
 from time import time
 
 N = 100 #how many numbers will be sorted
 
-def mySort(L):
-    #put your code here
-    return L
+'''procedure cocktailShakerSort( A : list of sortable items ) defined as:
+  do
+    swapped := false
+    for each i in 0 to length( A ) - 2 do:
+      if A[ i ] > A[ i + 1 ] then // test whether the two elements are in the wrong order
+        swap( A[ i ], A[ i + 1 ] ) // let the two elements change places
+        swapped := true
+      end if
+    end for
+    if not swapped then
+      // we can exit the outer loop here if no swaps occurred.
+      break do-while loop
+    end if
+    swapped := false
+    for each i in length( A ) - 2 to 0 do:
+      if A[ i ] > A[ i + 1 ] then
+        swap( A[ i ], A[ i + 1 ] )
+        swapped := true
+      end if
+    end for
+  while swapped // if no elements have been swapped, then the list is sorted
+end procedure'''
+
+def mySort(A):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(0,len(A)-1):
+            if A[i] > A[i+1]:
+                A[i], A[i+1] = A[i+1], A[i]
+    return A
 
 if __name__ == '__main__':
     

c83d178943cae1fe3b1ccc2ba4075772b31dd905
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-11-30 14:45:25 -0500

    Updated from Brython Server: 11/30/2017 2:45:25 PM

--- warmup15.py
+++ warmup15.py
@@ -1,1 +1,12 @@
+#Jack Robey
+#11/30/17
+#warmup15.py - doubles the numbers in a list
 
+def doubled(L):
+    L2 = []
+    for i in L:
+        L2.append(2*i)
+    return L2
+
+print(doubled([2,4,6]))
+    

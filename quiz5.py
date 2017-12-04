770e42a7a930a6bb36098e8ae6fde62eace4ae6c
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-12-04 14:47:34 -0500

    Updated from Brython Server: 12/4/2017 2:47:34 PM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,22 @@
+#Jack Robey
+#12/4/17
+#quiz5.py
 
+from random import randint
+
+def rand5():
+    x = []
+    for i in range(1,6):
+        x.append(randint(1,101))
+    return x
+
+def lastElement(y):
+    return y[len(y)-1]
+
+def biggest(z):
+    z.sort()
+    return z[len(z)-1]
+
+print(rand5())
+print(lastElement(['cat','dog','rat']))
+print(biggest([3,-1,5,-2,7,2,1]))
